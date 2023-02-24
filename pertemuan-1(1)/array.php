53e713d18c4de4b790ef0d969688306d0b4892aa
thirafi kazhimi yusuf <thirafi20@gmail.com>
2023-02-24 16:19:03 +0700

    hasil praktikum sesi 2

---
+++ pertemuan-1(1)/array.php
@@ -0,0 +1,8 @@
+<?php
+// Array Indexing
+$animals = ['Kambing', 'Kucing', 'Sapi'];
+
+// Mengakses Array Indexing
+foreach($animals as $animal){
+    echo $animal . '<br>';
+}
---
+++ pertemuan-1(1)/array_assosiatif.php
@@ -0,0 +1,10 @@
+<?php
+//Array Assosiatif
+$student = [
+    'name' => 'Thirafi Kazhimi Yusuf', 
+    'nim' => 110222290,
+    'major' => 'Teknik Informatika'
+];
+
+//Mengakses Array Assosiatif
+echo "Jurusan saya adalah = $student[major]";
